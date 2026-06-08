d713ee0997572018998f5f6759fa2e5d0d9259b1
CasperLCasper <64712311+CasperLCasper@users.noreply.github.com>
2026-06-08 23:36:07 +0300

    Create maintenance.js

---
+++ public/maintenance.js
@@ -0,0 +1,12 @@
+// ============================================ //
+// SYSTEM MAINTENANCE CONFIGURATION
+// ============================================ //
+
+window.MAINTENANCE_CONFIG = {
+  // 🛠️ NOMAINI UZ true, LAI IESLĒGTU APKOPES REŽĪMU // NOMAINI UZ false, LAI IZSLĒGTU APKOPES REŽĪMU
+  isMaintenance: true, 
+  
+  // Ziņojumi, ko lietotājs redzēs uz ekrāna
+  title: '🛠️ TEHNISKĀ APKOPE / MAINTENANCE 🛠️',
+  subtitle: 'Lapa uz brīdi nav pieejama. Atvainojamies par sagādātajām neērtībām.'
+};
